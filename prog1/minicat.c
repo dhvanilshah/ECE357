e526ee74211eebbc85bd01ca1332e0cd394b83ba
Dhvanil Shah <dhvanil.shah98@gmail.com>
2018-09-11 23:26:26 -0400

    first commit

---
+++ prog1/minicat.c
@@ -0,0 +1,13 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+    int buffersize = 0;
+    char *infile;
+    char *outfile;
+}
